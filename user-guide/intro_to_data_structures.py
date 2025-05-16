1b6a90652a9ee69468f55f5296eff90e03660c88
rhasanm <hasanrakibul.masum@gmail.com>
2025-05-16 22:54:40 +0600

    chore: file name changed

---
+++ user-guide/intro_to_data_structures.py
@@ -0,0 +1,14 @@
+import marimo
+
+__generated_with = "0.13.7"
+app = marimo.App(width="medium")
+
+
+@app.cell(hide_code=True)
+def _():
+    import marimo as mo
+    return
+
+
+if __name__ == "__main__":
+    app.run()
